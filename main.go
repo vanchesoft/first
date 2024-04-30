4557582f2911e8d22d4a22e70b550130008ccb2c
vanchesoft <145585165+vanchesoft@users.noreply.github.com>
2024-04-30 17:25:22 +0000

    news

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 	// categoryOrigin := []string{"motorhome"}
 	// categoryOrigin := []string{"random"}
 	// categoryOrigin := []string{"truck", "heavy", "bus", "trailer", "motorhome", "random"}
-	categoryOrigin := []string{"truck", "heavy", "bus", "trailer", "motorhome"}
+	categoryOrigin := []string{"bus"}
 
 	if encodedOnly {
 
